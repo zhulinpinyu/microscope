58422650f1d99b32d81f04ce349d0f8c06de7837
zhulinpinyu <mulixiang199016@gmail.com>
2016-03-07 11:38:42 +0800

    add seeds data to Posts Collection

---
+++ server/fixtures.js
@@ -0,0 +1,20 @@
+var postsData = [
+  {
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  },
+  {
+    title: 'Meteor',
+    url: 'http://meteor.com'
+  },
+  {
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+  }
+];
+
+if(Posts.find().count()===0){
+  postsData.forEach(function(p){
+    Posts.insert(p);
+  });
+}
