e050476b4d0eda8f79bcdb9344ec0a5d6b78b5b1
zhulinpinyu <mulixiang199016@gmail.com>
2016-03-15 20:54:07 +0800

    validate post for edit

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,3 +1,14 @@
+Template.postEdit.onCreated(function(){
+  Session.set('postEditErrors',{});
+});
+Template.postEdit.helpers({
+  errorMessage: function(field){
+    return Session.get('postEditErrors')[field];
+  },
+  errorClass: function(field){
+    return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+  }
+});
 Template.postEdit.events({
   'submit form': function(e){
     e.preventDefault();
@@ -8,9 +19,14 @@ Template.postEdit.events({
       title: $(e.target).find('[name=title]').val()
     }
 
+    var errors = validatePost(postProperties);
+    if(errors.title||errors.url){
+      return Session.set('postEditErrors',errors);
+    }
+
     Posts.update(currentPostId,{$set: postProperties},function(error){
       if(error){
-        throwError(error)
+        throwError(error.reason)
       }else{
         Router.go('postPage',{_id: currentPostId});
       }
