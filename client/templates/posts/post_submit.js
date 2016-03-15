24dddbd0aa589b0467ceb306605c3a022acc809f
zhulinpinyu <mulixiang199016@gmail.com>
2016-03-15 20:47:29 +0800

    validate

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,14 @@
+Template.postSubmit.onCreated(function(){
+  Session.set('postSubmitErrors',{});
+});
+Template.postSubmit.helpers({
+  errorMessage: function(field){
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function(field){
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
 Template.postSubmit.events({
   'submit form': (e) => {
     e.preventDefault();
@@ -6,6 +17,11 @@ Template.postSubmit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
+
+    var errors = validatePost(post);
+    if(errors.title||errors.url){
+      return Session.set('postSubmitErrors',errors);
+    }
 
     Meteor.call('postInsert',post,function(error,result){
       if(error){
